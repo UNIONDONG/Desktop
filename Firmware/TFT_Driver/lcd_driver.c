78fd50f5b7a4e48202112eb28ac71f3195b47d87
dongxihang <xihang.dong@amicro.com.cn>
2023-12-21 18:36:38 +0800

    tft_driver : v0.4

--- Firmware/TFT_Driver/lcd_driver.c
+++ Firmware/TFT_Driver/lcd_driver.c
@@ -73,7 +73,7 @@ static void tft_write_command(LCD_DEVICE *tft_dev, uint8_t command)
 
 	trans = kzalloc(sizeof(struct spi_transfer), GFP_KERNEL);
 	if (IS_ERR_OR_NULL(trans)) {
-		pr_err("alloc spi transfer failed.\r\n");
+		pr_err("alloc spi transfer failed.\n");
 		return;
 	}
 
@@ -96,7 +96,7 @@ static void tft_write_data(LCD_DEVICE *tft_dev, uint8_t data)
 	uint8_t txdata = data;
 	trans = kzalloc(sizeof(struct spi_transfer), GFP_KERNEL);
 	if (IS_ERR_OR_NULL(trans)) {
-		pr_err("alloc spi transfer failed.\r\n");
+		pr_err("alloc spi transfer failed.\n");
 		return;
 	}
 
@@ -121,7 +121,7 @@ static void tft_write_data_16bit(LCD_DEVICE *tft_dev, uint16_t data)
 
 	trans = kzalloc(sizeof(struct spi_transfer), GFP_KERNEL);
 	if (IS_ERR_OR_NULL(trans)) {
-		pr_err("alloc spi transfer failed.\r\n");
+		pr_err("alloc spi transfer failed.\n");
 		return;
 	}
 
@@ -298,7 +298,7 @@ void tft_set_gram_scanway(LCD_DEVICE *tft_dev, LCD_SCAN_DIR Scan_dir)
 
 static void tft_init(LCD_DEVICE *tft_dev)
 {
-	pr_info("lcd backlight pin: %d\r\n", tft_dev->lcd_pin_backlight);
+	pr_info("lcd backlight pin: %d\n", tft_dev->lcd_pin_backlight);
 	// backlight setting
 	LCD_PIN_SET_HIGH(tft_dev->lcd_pin_backlight);
 
@@ -429,7 +429,7 @@ static void lcd_clean(LCD_DEVICE *tft_dev, uint16_t Color)
 //
 //     mdelay(10);
 //
-//     printk(KERN_ALERT "LCD display picture ok!\r\n");
+//     printk(KERN_ALERT "LCD display picture ok!\n");
 // }
 
 
@@ -441,7 +441,7 @@ static int tft_open(struct inode *inode, struct file *file)
 	// LCD_DEVICE *tft_dev = lcd_tft;
 	file->private_data = tft_dev;
 
-	pr_info("origin addr: 0x%x , current addr : 0x%x\r\n", 0, tft_dev->lcd_spi_dev);
+	pr_info("origin cdev addr: 0x%x , current addr : 0x%x\n", tft_cdev, &tft_dev->lcd_cdev);
 	pr_info("lcd init ......");
 	// tft_init(tft_dev);
 	// lcd_clean(tft_dev, BLACK);
@@ -456,7 +456,7 @@ static int tft_write(struct file *filp, const char __user *buf, size_t cnt, loff
 	uint16_t *Image;
 	Image = (uint16_t *)kzalloc(cnt, GFP_KERNEL);
 	num = copy_from_user(Image, buf, cnt);
-	pr_info("write ......\r\n");
+	pr_info("write ......\n");
     for(i = 0; i < cnt / 2; i++)
     {
         tft_write_data_16bit(tft_dev, Image[i]);
@@ -469,8 +469,8 @@ static int tft_write(struct file *filp, const char __user *buf, size_t cnt, loff
 static int tft_release(struct inode *inode, struct file *filp)
 {
 	LCD_DEVICE *tft_dev = filp->private_data;
-	pr_info("origin addr: 0x%x , current addr : 0x%x\r\n", lcd_tft, tft_dev);
-	pr_info("exit\r\n");
+	pr_info("origin addr: 0x%x , current addr : 0x%x\n", lcd_tft, tft_dev);
+	pr_info("exit\n");
 	// lcd_clean(tft_dev, WHITE);
 	return 0;
 }
@@ -494,51 +494,51 @@ static int lcd_hardware_reset(LCD_DEVICE *lcd_dev)
 	// dts prase, in order to get pins
 	lcd_dev->lcd_dev_node = of_find_node_by_name(NULL, "ecspi_tft");
     if(lcd_dev->lcd_dev_node == NULL) {
-        pr_err("get lcd device node failed\r\n");
+        pr_err("get lcd device node failed\n");
 		return -1;
     }
 
     lcd_dev->lcd_pin_dc = of_get_named_gpio(lcd_dev->lcd_dev_node, "dc-pin", 0);
 	if (IS_ERR_VALUE(lcd_dev->lcd_pin_dc)) {
-		pr_info("get pin_dc error\r\n");
+		pr_info("get pin_dc error\n");
 		return -1;
 	} else {
-		pr_info("lcd_pin_dc is %d\r\n", lcd_dev->lcd_pin_dc);
+		pr_info("lcd_pin_dc is %d\n", lcd_dev->lcd_pin_dc);
 		gpio_direction_output(lcd_dev->lcd_pin_dc, 1);
 	}
         
     lcd_dev->lcd_pin_reset = of_get_named_gpio(lcd_dev->lcd_dev_node, "reset-pin", 0);
 	if (IS_ERR_VALUE(lcd_dev->lcd_pin_reset)) {
-		pr_info("get pin_reset error\r\n");
+		pr_info("get pin_reset error\n");
 		return -1;
 	} else {
-		pr_info("lcd_pin_reset is %d\r\n", lcd_dev->lcd_pin_reset);
+		pr_info("lcd_pin_reset is %d\n", lcd_dev->lcd_pin_reset);
 		gpio_direction_output(lcd_dev->lcd_pin_reset, 1);
 	}
 
     lcd_dev->lcd_pin_backlight = of_get_named_gpio(lcd_dev->lcd_dev_node, "backlight-pin", 0);
 	if (IS_ERR_VALUE(lcd_dev->lcd_pin_backlight)) {
-		pr_info("get pin_backlight error\r\n");
+		pr_info("get pin_backlight error\n");
 		return -1;
 	} else {
-		pr_info("lcd_pin_backlight is %d\r\n", lcd_dev->lcd_pin_backlight);
+		pr_info("lcd_pin_backlight is %d\n", lcd_dev->lcd_pin_backlight);
 		gpio_direction_output(lcd_dev->lcd_pin_backlight, 1);
 	}
 
     ret = of_property_read_u32_index(lcd_dev->lcd_dev_node, "lcd-width", 0, &lcd_dev->lcd_width);
 	if (ret < 0) {
-		pr_info("get lcd width error\r\n");
+		pr_info("get lcd width error\n");
 		return -1;
 	} else {
-		pr_info("lcd width is %d\r\n", lcd_dev->lcd_width);
+		pr_info("lcd width is %d\n", lcd_dev->lcd_width);
 	}
 
     ret = of_property_read_u32_index(lcd_dev->lcd_dev_node, "lcd-height", 0, &lcd_dev->lcd_height);
 	if (ret < 0) {
-		pr_info("get lcd_height error\r\n");
+		pr_info("get lcd_height error\n");
 		return -1;
 	} else {
-		pr_info("lcd_height is %d\r\n", lcd_dev->lcd_height);
+		pr_info("lcd_height is %d\n", lcd_dev->lcd_height);
 	}
 
 	return 0;
@@ -548,10 +548,11 @@ static int lcd_probe(struct spi_device *spi)
 {
     int ret = 0;
 	LCD_DEVICE *lcd_dev;
-    pr_info("lcd driver probe\r\n");
+
+    pr_info("lcd driver probe\n");
 	lcd_dev = kzalloc(sizeof(LCD_DEVICE), GFP_KERNEL);
 	if (IS_ERR_OR_NULL(lcd_dev)) {
-		pr_err("kzalloc error!\r\n");
+		pr_err("kzalloc error!\n");
 		ret = -ENOMEM;
 		goto alloc_err;
 	}
@@ -561,51 +562,67 @@ static int lcd_probe(struct spi_device *spi)
 
 	// spi config
 	lcd_dev->lcd_spi_dev = spi;
-	pr_info("origin addr: 0x%x , current addr : 0x%x\r\n", spi, lcd_dev->lcd_spi_dev);
+	pr_info("origin addr: 0x%x , current addr : 0x%x\n", spi, lcd_dev->lcd_spi_dev);
 
 	lcd_dev->lcd_spi_dev->mode = SPI_MODE_0;
 	lcd_dev->lcd_spi_dev->max_speed_hz = 20000000;
     spi_setup(lcd_dev->lcd_spi_dev);
+	spi_set_drvdata(lcd_dev->lcd_spi_dev, lcd_dev);
 	
 	// char dev create
     ret = alloc_chrdev_region(&lcd_dev->dev_number, 0, DEV_CNT, DEV_NAME);
     if(ret < 0) {
-        pr_err("alloc lcd device number failed\r\n");
+        pr_err("alloc lcd device number failed\n");
         goto alloc_err;
     }
-	pr_info("lcd_device_number: %x\r\n", lcd_dev->dev_number);
+	pr_info("lcd_device_number: %x\n", lcd_dev->dev_number);
 	// lcd_dev->lcd_cdev = cdev_alloc();
     lcd_dev->lcd_cdev.owner = THIS_MODULE;
     cdev_init(&lcd_dev->lcd_cdev, &tft_char_dev_ops);
     ret = cdev_add(&lcd_dev->lcd_cdev, lcd_dev->dev_number, DEV_CNT);
     if(ret < 0) {
-        pr_err("add cdev failed, err_code : %d \r\n", ret);
+        pr_err("add cdev failed, err_code : %d \n", ret);
         goto add_err;
     }
 	// class create
     lcd_dev->lcd_class = class_create(THIS_MODULE, DEV_NAME);
+	if (IS_ERR_OR_NULL(lcd_dev->lcd_class)) {
+		pr_err("class create error!\n");
+		ret = PTR_ERR(lcd_dev->lcd_class);
+		goto class_err;
+	}
     lcd_dev->lcd_device = device_create(lcd_dev->lcd_class, NULL, lcd_dev->dev_number, NULL, DEV_NAME);
+	if (IS_ERR(lcd_dev->lcd_device)) {
+		pr_err("Failed to create device\n");
+		ret = PTR_ERR(lcd_dev->lcd_device);
+		goto device_err;
+	}
 
-	pr_info("max_speed_hz = %d\r\n", lcd_dev->lcd_spi_dev->max_speed_hz);
-	pr_info("chip_select = %d\r\n", (int)lcd_dev->lcd_spi_dev->chip_select);
-	pr_info("bits_per_word = %d\r\n", (int)lcd_dev->lcd_spi_dev->bits_per_word);
-	pr_info("mode = %d\r\n", lcd_dev->lcd_spi_dev->mode);
+	pr_info("max_speed_hz = %d\n", lcd_dev->lcd_spi_dev->max_speed_hz);
+	pr_info("chip_select = %d\n", (int)lcd_dev->lcd_spi_dev->chip_select);
+	pr_info("bits_per_word = %d\n", (int)lcd_dev->lcd_spi_dev->bits_per_word);
+	pr_info("mode = %d\n", lcd_dev->lcd_spi_dev->mode);
 	
   	return 0;
 
+device_err:
+	class_destroy(lcd_dev->lcd_class);
+
+class_err:
+	cdev_del(&lcd_dev->lcd_cdev);
+
 add_err:
 	unregister_chrdev_region(lcd_dev->dev_number, DEV_CNT);
-	pr_err("add char dev error \r\n");
 	
 alloc_err:
-	return -1;
+	return ret;
 }
 
-static int lcd_remove(struct spi_device *spi) {
+static int lcd_remove(struct spi_device *spi)
+{
+	LCD_DEVICE *lcd_dev = spi_get_drvdata(spi);
 
-	LCD_DEVICE *lcd_dev = container_of(&spi, LCD_DEVICE, lcd_spi_dev);
-
-	pr_info("origin addr: 0x%x , current addr : 0x%x\r\n", spi, lcd_dev->lcd_spi_dev);
+	pr_info("origin addr: 0x%x , current addr : 0x%x\n", spi, lcd_dev->lcd_spi_dev);
 	pr_info("lcd driver remove");
 	device_destroy(lcd_dev->lcd_class, lcd_dev->dev_number);
 	class_destroy(lcd_dev->lcd_class);
@@ -632,12 +649,12 @@ struct spi_driver lcd_driver = {
 static int __init lcd_driver_init(void) {
     int status;
     status = spi_register_driver(&lcd_driver);
-    pr_info("lcd_driver_init status : %d\r\n", status);
+    pr_info("lcd_driver_init status : %d\n", status);
     return status;
 }
 
 static void __exit lcd_driver_exit(void) {
-    pr_info("lcd_driver_exit\r\n");
+    pr_info("lcd_driver_exit\n");
     spi_unregister_driver(&lcd_driver);
 }
 
